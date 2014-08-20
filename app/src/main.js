b665ca7d797a6a10e934b08c809ef03b4b61dc5a
Chai Phonbopit <phonbopit@gmail.com>
2014-08-20 13:49:18 +0700

    add content - Hello Famo.us

--- app/src/main.js
+++ app/src/main.js
@@ -1,31 +1,19 @@
 /* globals define */
 define(function(require, exports, module) {
     'use strict';
-    // import dependencies
-    var Engine = require('famous/core/Engine');
-    var Modifier = require('famous/core/Modifier');
-    var Transform = require('famous/core/Transform');
-    var ImageSurface = require('famous/surfaces/ImageSurface');
 
-    // create the main context
+    // Use Famo.us Engine
+    var Engine = require('famous/core/Engine');
+    var Surface = require('famous/core/Surface');
+
+    // Create new Contenxt
     var mainContext = Engine.createContext();
 
-    // your app here
-    mainContext.setPerspective(1000);
-
-    var logo = new ImageSurface({
-        size: [200, 200],
-        content: '/content/images/famous_logo.png',
-        classes: ['backfaceVisibility']
+    // Create Surface object with content.
+    var firstSurface = new Surface({
+        content: 'Hello Famo.us \m/'
     });
 
-    var initialTime = Date.now();
-    var centerSpinModifier = new Modifier({
-        origin: [0.5, 0.5],
-        transform : function() {
-            return Transform.rotateY(.002 * (Date.now() - initialTime));
-        }
-    });
-
-    mainContext.add(centerSpinModifier).add(logo);
+    // Add Surface to mainContext
+    mainContext.add(firstSurface);
 });
