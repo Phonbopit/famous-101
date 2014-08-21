c75c06f17ea51dd19bb877b973617f72b150e48b
Chai Phonbopit <phonbopit@gmail.com>
2014-08-21 15:51:15 +0700

    finish lesson3

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Open [localhost:1337](http://localhost:1337/) That's all \m/
 
 - [x] [Lesson 1 : Displaying Content](https://famo.us/university/famous-101/displaying/1/) 
 - [x] [Lesson 2 : Positioning Elements](https://famo.us/university/famous-101/positioning)
-- [ ] [Lesson 3 : Animating Elements](https://famo.us/university/famous-101/animating/)
+- [x] [Lesson 3 : Animating Elements](https://famo.us/university/famous-101/animating/)
 - [ ] [Lesson 4 : Handling Events](https://famo.us/university/famous-101/eventing/)
 - [ ] [Lesson 5 : Famo.us Starter Kit](https://famo.us/university/famous-101/starter-kit/)
 - [ ] [Project : Slideshow](https://famo.us/university/famous-101/slideshow/)
--- app/src/lesson3.js
+++ app/src/lesson3.js
@@ -1,19 +1,54 @@
+/* Lesson 3 : Creating Animation */
+
 define(function(require, exports, module) {
 	'use strict';
 
 	var Engine = require('famous/core/Engine');
 	var Surface = require('famous/core/Surface');
+	var Transform  = require('famous/core/Transform');
+	var StateModifier = require('famous/modifiers/StateModifier');
+	var Easing = require('famous/transitions/Easing');
 
 	var context = Engine.createContext();
 
 	var surface = new Surface({
+		size: [100, 100],
 		content: 'Simple Test',
 		properties: {
-			size: [100, 100],
+			color: '#ededed',
 			textAlign: 'center',
 			backgroundColor: '#df0932'
 		}
 	});
 
-	context.add(surface);
+	var stateModifier = new StateModifier({
+		origin: [0.5, 0]
+	});
+
+	context.add(stateModifier).add(surface);
+
+	// Handler onClick on surface
+	surface.on('click', function() {
+		stateModifier.halt();
+		this.setContent('halted');
+		stateModifier.setTransform(
+			Transform.translate(300, 0, 0),
+			{ duration: 1000, curse: Easing.inBounce }
+		);
+	});
+
+	// Use physics Transitions
+	var Transitionable = require('famous/transitions/Transitionable');
+	var SpringTransition = require('famous/transitions/SpringTransition');
+	Transitionable.registerMethod('ss', SpringTransition); // ss stand for spring
+
+	var spring = {
+		method : 'ss',
+		period : 1000,
+		dampingRadio: 0.3
+	};
+
+	stateModifier.setTransform(
+		Transform.translate(0, 300, 0), spring
+	);
 });
